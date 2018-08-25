0b87eec0ea6b665eec4a01fe75315cd7f283d0cb
Evan Webb <webbevana@gmail.com>
2018-08-25 12:49:07 -0400

    working footer component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import NavigationBar from '@/components/NavigationBar.vue'
+import footer from '@/components/footer.vue'
 
 Vue.use(Router)
 
@@ -9,13 +10,14 @@ window.Vue = require('vue')
 
 // Vue Components
 Vue.component('navigationbar', NavigationBar)
+Vue.component('footer', footer)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'NavBar',
-      component: NavigationBar
+      name: 'footer',
+      component: footer
     }
   ]
 })
