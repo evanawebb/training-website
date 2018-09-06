d4ac9707010a62deb020fb21a54946a46d378ee5
Evan Webb <webbevana@gmail.com>
2018-09-05 21:57:30 -0400

    Adds map to contact page
    Not currently functioning but just needs the correct coordinates and API Key

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="tile is-parent">
+    <article class="tile is-child notification is-danger">
+    <p class="title">Contact Us</p>
+    <p class="subtitle">Aligned with the right tile</p>
+    <div class="content">
+        <GmapMap
+            :center="{lat:10, lng:10}"
+            :zoom="7"
+            map-type-id="terrain"
+            style="width: 500px; height: 300px"
+        >
+            <GmapMarker
+                :key="index"
+                v-for="(m, index) in markers"
+                :position="m.position"
+                :clickable="true"
+                :draggable="true"
+                @click="center=m.position"
+            />
+        </GmapMap>
+    </div>
+    </article>
+</div>
+</template>
+
+
+<script>
+
+</script>
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,16 @@ import Router from 'vue-router'
 import NavigationBar from '../components/NavigationBar.vue'
 import home from '../components/home.vue'
 import SocialMedia from '../components/SocialMedia.vue'
+import ContactUs from '../components/ContactUs.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(Router)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'YOUR_API_TOKEN',
+    libraries: 'places'
+  }
+})
 
 // components
 window.Vue = require('vue')
@@ -13,6 +21,7 @@ window.Vue = require('vue')
 Vue.component('navigationbar', NavigationBar)
 Vue.component('home', home)
 Vue.component('socialmedia', SocialMedia)
+Vue.component('contact', ContactUs)
 
 export default new Router({
   routes: [
@@ -23,10 +32,14 @@ export default new Router({
     },
     {
       path: '/social',
-      name: 'social',
+      name: 'Social',
       component: SocialMedia
 
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: ContactUs
     }
-
   ]
 })
